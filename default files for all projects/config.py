e076297c6b92e35c20cc4215b083960dc7a037a6
Vishnu vardhan Varapalli <52121721+VishnuVardhanVarapalli@users.noreply.github.com>
2021-01-01 18:22:28 +0530

    Update config.py

--- default files for all projects/config.py
+++ default files for all projects/config.py
@@ -1,15 +1,3 @@
-import argparse
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+TRAINING_FILE = "GIVE PATH OF YOUR INPUT FOLDER"
 
-    parser.add_argument("--training_file",type = str)
-    parser.add_argument("--models",type = str)
-    
-    args = parser.parse_args()
-    
-    #training file contains input 
-    TRAINING_FILE = args.training_file
-    
-    #Directory for saving output models
-    MODELS  = args.models
-
+MODELS = "GIVE PATH OF FOLDERS, WHERE MODELS NEED TO BE SAVED"
